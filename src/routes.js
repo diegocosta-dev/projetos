2544ef7cc9c6cf6a90df59849e132dc706f8e899
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2020-11-25 11:59:32 -0300

    TestPush

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,20 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Conversor from './Components/Conversor/Conversor'
-import Home from './Components/Outros/Home'
-import Calculadora from './Components/Calculadora/Calculadora'
-import GerenciadorDeTarefas from './Components/GerenciadorDeTarefas/GerenciadorDeTarefas'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Conversor from './Components/Conversor/Conversor';
+import Home from './Components/Outros/Home';
+import Calculadora from './Components/Calculadora/Calculadora';
+import GerenciadorDeTarefas from './Components/GerenciadorDeTarefas/GerenciadorDeTarefas';
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/conversor" component={Conversor}/>
-                <Route path="/calculadora" component={Calculadora}/>
-                <Route path="/gerenciador" component={GerenciadorDeTarefas}/>
-            </Switch>
-        </BrowserRouter>
-    )
-    
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/conversor" component={Conversor} />
+        <Route path="/calculadora" component={Calculadora} />
+        <Route path="/gerenciador" component={GerenciadorDeTarefas} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default Router
+export default Router;
